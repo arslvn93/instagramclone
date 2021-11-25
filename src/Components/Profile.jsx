4d72419d2d2b1e746d6b75d977dcad82afc22d5c
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2021-11-25 14:03:14 -0500

    description change

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -37,7 +37,7 @@ export default function Profile() {
                         </div>
                     </div>
                 </div>
-                <p class='description pt-2 w-100 mx-4 d-md-none d-block' style={{paddingRight: '30px'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                <p class='description pt-2 w-100 mx-4 d-md-none d-block' style={{paddingRight: '30px'}}>Click on the images below to view the full listing details! </p>
                 <div style={{border: '1px solid rgba(0,0,0,0.1)'}} class="mt-3 w-75 mx-auto" />
                 <div style={{borderTop: '1px solid black', width: 70}} class="d-flex mx-auto align-items-center justify-content-center pt-4">
                     <svg aria-label="" class="_8-yf5 " color="#262626" fill="#262626" height="12" role="img" viewBox="0 0 24 24" width="12"><rect fill="none" height="18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" width="18" x="3" y="3"></rect><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="9.015" x2="9.015" y1="3" y2="21"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="14.985" x2="14.985" y1="3" y2="21"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="9.015" y2="9.015"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="14.985" y2="14.985"></line></svg>
